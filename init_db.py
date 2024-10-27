166c207c51a56bd5adafe46c7241889aed6eebf4
Surya Togaru <stogaru@andrew.cmu.edu>
2024-10-27 16:44:05 -0400

    refactos DB init out of the app

--- init_db.py
+++ init_db.py
@@ -9,9 +9,9 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)''')
-    c.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('admin', 'admin')")
-    c.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('student', 'tartans')")
+    c.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, is_admin INTEGER)''')
+    c.execute("INSERT OR IGNORE INTO users (username, password, is_admin) VALUES ('admin', 'admin', 1)")
+    c.execute("INSERT OR IGNORE INTO users (username, password, is_admin) VALUES ('student', 'tartans', 0)")
     conn.commit()
     conn.close()
 
