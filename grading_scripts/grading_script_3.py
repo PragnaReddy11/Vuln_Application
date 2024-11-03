bf1ba5e5dde8e9ea728fbfe9f9842795bb35a34f
Surya Togaru <stogaru@andrew.cmu.edu>
2024-11-03 05:42:21 -0500

    Adds grading script 5

--- grading_scripts/grading_script_3.py
+++ grading_scripts/grading_script_3.py
@@ -5,23 +5,23 @@ RESULT_JSON_PATH = "/home/student/gr8scope/semgrep/"
 
 def check_scan_errors(scan: dict):
     if len(scan.get("errors")) != 0:
-        print("Scan failed with errors.\nPlease re-run the scan properly (+0).")
+        print("Scan failed with errors.")
         return
-    print("Scan completed without errors (+10).")
+    print("Scan successfully executed.")
 
 def check_paths_scanned(scan: dict):
     paths_scanned = scan.get("paths").get("scanned")
     if len(paths_scanned) == 1 and paths_scanned[0].endswith("app.py"):
-        print("Semgrep scanned your code successfully (+10).")
+        print("Code was successfully scanned.")
         return
-    print("Semgrep was not invoked with ")
+    print("Wrong file was scanned with semgrep.")
 
 def check_results(scan: dict):
     results = scan.get("results")
     if len(results) > 0 and results[0].get("check_id").endswith("sql-injection"):
-        print("Your custom semgrep rule detected a SQL injection. Hooray!")
+        print("SQL injection was successfully detected. Hooray!")
         return
-    print("")
+    print("Failed to detect SQL injection.")
 
 if __name__ == "__main__":
     with open(RESULT_JSON_PATH, "r") as f:
