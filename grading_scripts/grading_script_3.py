501cde81f2169edd98776bd54c968e68b322e632
stogaru <143449212+stogaru@users.noreply.github.com>
2024-11-18 02:30:31 -0800

    Modifies gs3 and gs4

--- grading_scripts/grading_script_3.py
+++ grading_scripts/grading_script_3.py
@@ -4,23 +4,23 @@ RESULT_PATH = "/home/student/gr8scope/semgrep/results.json"
 
 def check_scan_errors(scan: dict):
     if len(scan.get("errors")) != 0:
-        print("Scan failed with errors.")
+        print("❌ Check #1 - Semgrep scan failed with errors: 0 points.")
         return
-    print("Scan successfully executed.")
+    print("✅ Check #2 - Semgrep scan has no errors: +10 points.")
 
 def check_paths_scanned(scan: dict):
     paths_scanned = scan.get("paths").get("scanned")
     if len(paths_scanned) == 1 and paths_scanned[0].endswith("app.py"):
-        print("Code was successfully scanned.")
+        print("✅ Check #2 - Semgrep scanned app.py: +10 points.")
         return
-    print("Wrong file was scanned with semgrep.")
+    print("❌ Check #2 - Semgrep did not scan app.py: 0 points.")
 
 def check_results(scan: dict):
     results = scan.get("results")
     if len(results) > 0 and results[0].get("check_id").endswith("sql-injection"):
-        print("SQL injection was successfully detected. Hooray!")
+        print("✅ Check #3 - Semgrep detected SQLi: +10 points.")
         return
-    print("Failed to detect SQL injection.")
+    print("❌ Check #3 - Semgrep did not detect SQLi: 0 points.")
 
 if __name__ == "__main__":
     with open(RESULT_PATH, "r") as f:
