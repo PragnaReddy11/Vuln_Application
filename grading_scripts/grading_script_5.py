e644322e3388d7a7b8ca3edaa61950d00497bcf8
stogaru <143449212+stogaru@users.noreply.github.com>
2024-11-18 04:39:24 -0800

    Modifies GSes

---
+++ grading_scripts/grading_script_5.py
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Variables
+PUBLIC_KEY="/home/student/keys/cosign.pub"
+REGISTRY_USERNAME="student"
+REGISTRY_PASSWORD="tartans@1"
+IMAGE_NAME="gr8scope_$IMAGE_TAG"
+
+cosign verify \ 
+    --key "$PUBLIC_KEY" \
+    --allow-http-registry=true \
+    --insecure-ignore-tlog=true \
+    --registry-username="$REGISTRY_USERNAME" \
+    --registry-password="$REGISTRY_PASSWORD" \
+    docker-registry.local:5000/gr8scope:1.0.3-beta > cosign_output.txt 2>&1
+
+if grep -q "The cosign claims were validated" cosign_output.txt && \
+   grep -q "The signatures were verified against the specified public key" cosign_output.txt; then
+  echo "✅ Check #1 - Verified signatures on registry image: +10 points."
+else
+  echo "❌ Check #1 - Registry image's signature verification failed: 0 points."
+  cat cosign_output.txt
+fi
+
+cosign verify \
+    --key "$PUBLIC_KEY" \
+    --local-image ~/gr8scope_1.0.3-beta/ \
+    --registry-username="$REGISTRY_USERNAME" \
+    --registry-password="$REGISTRY_PASSWORD" \
+    --insecure-ignore-tlog=true > cosign_output.txt 2>&1
+
+# Check if the verification was successful
+if grep -q "The cosign claims were validated" cosign_output.txt && \
+   grep -q "The signatures were verified against the specified public key" cosign_output.txt; then
+  echo "✅ Check #2 - Verified signatures on local image: +10 points."
+  exit 0
+else
+  echo "❌ Check #2 - Local image's signature verification failed: 0 points."
+  cat cosign_output.txt
+  exit 1
+fi
